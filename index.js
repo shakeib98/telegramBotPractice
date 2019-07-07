111f9d18f2da76afe930b1e27be82bab5fab69e7
shakeib98 <shakeib98@outlook.com>
2019-07-07 13:25:34 +0500

    telegram bot one functionality added

---
+++ index.js
@@ -0,0 +1,12 @@
+const TelegramBot = require('node-telegram-bot-api')
+const token = '893235715:AAFHwINY2Ostw1eMtKixg8yUvS2Ypr3ZHrc'
+const bot = new TelegramBot(token, {polling: true})
+
+bot.on('message', (msg) => {
+
+    var Hi = "hi";
+    if (msg.text.toString().toLowerCase().indexOf(Hi) === 0) {
+        bot.sendMessage(msg.chatId,"Hello dear user");
+    }
+
+});
