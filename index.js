be17cbb42ba5aab6e8482ddc5b9b03d6c07fbdb5
shakeib98 <shakeib98@outlook.com>
2019-07-07 13:38:42 +0500

    more messages added

--- index.js
+++ index.js
@@ -7,6 +7,13 @@ bot.on('message', (msg) => {
     var Hi = "hi";
     if (msg.text.toString().toLowerCase().indexOf(Hi) === 0) {
         bot.sendMessage(msg.chat.id,"Hello dear " +msg.chat.first_name);
+        bot.sendMessage(msg.chat.id,"How are you?")
+
+        bot.on('message',(msg)=>{
+            if((msg.text.toString().toLowerCase().indexOf("I am fine") ===0) || (msg.text.toString().toLowerCase().indexOf("I'm fine") ===0) || (msg.text.toString().toLowerCase().indexOf("fine") ===0) || (msg.text.toString().toLowerCase().indexOf("great") ===0) || (msg.text.toString().toLowerCase().indexOf("Okay") ===0) ){
+                bot.sendMessage(msg.chat.id,"Great, What can I do for you?")
+            }
+        })
     }
 
 });
