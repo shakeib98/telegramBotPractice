ae491d10c1bfaff18f907d7e2fab63261caa9444
shakeib98 <shakeib98@outlook.com>
2019-07-07 13:50:11 +0500

    buttons added

--- index.js
+++ index.js
@@ -2,18 +2,23 @@ const TelegramBot = require('node-telegram-bot-api')
 const token = '893235715:AAFHwINY2Ostw1eMtKixg8yUvS2Ypr3ZHrc'
 const bot = new TelegramBot(token, {polling: true})
 
-bot.on('message', (msg) => {
-
-    var Hi = "hi";
-    if (msg.text.toString().toLowerCase().indexOf(Hi) === 0) {
-        bot.sendMessage(msg.chat.id,"Hello dear " +msg.chat.first_name);
-        bot.sendMessage(msg.chat.id,"How are you?")
-
-        bot.on('message',(msg)=>{
-            if((msg.text.toString().toLowerCase().indexOf("I am fine") ===0) || (msg.text.toString().toLowerCase().indexOf("I'm fine") ===0) || (msg.text.toString().toLowerCase().indexOf("fine") ===0) || (msg.text.toString().toLowerCase().indexOf("great") ===0) || (msg.text.toString().toLowerCase().indexOf("Okay") ===0) ){
-                bot.sendMessage(msg.chat.id,"Great, What can I do for you?")
-            }
+bot.onText(/start/, (msg, match) => {
+    console.log(msg);
+    const opts = {
+        reply_to_message_id: msg.message_id,
+        reply_markup: JSON.stringify({
+            keyboard: [
+                ['How are you?'],
+                ['Add two random numbers'],
+                ['What date is today?']
+                //     ['price'],
+                // ['height']
+            ],
+            'one_time_keyboard': true
         })
-    }
+    };
+    if(msg.chat.type=="private")
+        bot.sendMessage(msg.chat.id, 'Hi. I am SSBot, I am a bot made by Shakeib just for fun and practice', opts);
 
 });
+
